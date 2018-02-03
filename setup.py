fb1161e3b1b9b7c89b8cc48cc803f8f54fd86457
Giovanni Lanzani <giovanni@lanzani.nl>
2018-02-03 09:21:43 +0100

    Add setup file for easier install

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="cryptrage",
+    description="Download ticks from various cryptexchanges and alert on arbitrage",
+    author="Giovanni Lanzani",
+    install_requires=['pytz', 'tzlocal', 'krakenex', 'gdax', 'BitstampClient',
+                      'psycopg2', 'pid'],
+    extras_require={
+        'dev': ['pytest'],
+        'test': ['pytest'],
+    },
+    packages=find_packages()
+)
