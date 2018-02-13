4928dbd530e4487497d9c068043cd1a88d91d15d
Giovanni Lanzani <giovanni@lanzani.nl>
2018-02-13 10:15:48 +0100

    Rewrite the configuration changer

--- configure/configure.db.py
+++ configure/configure.db.py
@@ -1,11 +1,11 @@
+import configparser
 
-conf_file = '/etc/yum.repos.d/CentOS-Base.repo'
 
-with open(conf_file, 'r') as f:
-    buf = f.readlines()
-
-with open(conf_file, 'w') as f:
-    for line in buf:
-        if line == "[base]\n" or line == "[updates]\n":
-            line = line + "exclude=postgresql*\n"
-        f.write(line)
+for file, sections in zip(['/etc/yum.repos.d/CentOS-Base.repo', '/etc/yum/yum-cron.conf'],
+                          [['base', 'updates'], ['base']]):
+    config = configparser.ConfigParser()
+    config.read(file)
+    for section in sections:
+        config[section]['exclude'] += ' postgresql* '
+    with open(file, 'w') as f:
+        config.write(f)
--- configure/configure.db.sh
+++ configure/configure.db.sh
@@ -4,10 +4,11 @@
 sudo python configure.db.py
 sudo yum install -y wget
 
-LOCATION="https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/pgdg-centos10-10-2.noarch.rpm"
+DATA_FOLDER=/var/lib/pgsql/10/data
+LOCATION="https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/pgdg-centos10-10-1.noarch.rpm"
 
 sudo yum install -y $LOCATION
-sudo yum install -y postgresql10-server
+sudo yum install -y postgresql10-server-10.1  # there is a compatibility issue with 10.2
 
 sudo /usr/pgsql-10/bin/postgresql-10-setup initdb
 sudo systemctl enable postgresql-10.service
@@ -30,7 +31,7 @@ PACKAGE="timescaledb-0.8.0-postgresql-10-0.x86_64.rpm"
 wget "https://timescalereleases.blob.core.windows.net/rpm/$PACKAGE"
 sudo yum install -y $PACKAGE
 
-echo "shared_preload_libraries = 'timescaledb'"  | sudo tee -a $DATA_FOLDER/pg_hba.conf
-sudo systemctl restart postgresql-10.service
+echo "shared_preload_libraries = 'timescaledb'"  | sudo tee -a $DATA_FOLDER/postgresql.conf
 
+sudo systemctl restart postgresql-10.service
 sudo -n -u postgres psql -d timescale -c "CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE"
